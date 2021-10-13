26f0c310e6d9aa11ca3e050f89a229e4917b2674
Amir <Amir.yazidi1@esprit.tn>
2021-10-13 17:44:49 +0100

    FixingModulEntreprise

--- src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
@@ -30,7 +30,7 @@ public class RestControlEntreprise {
 	ITimesheetService itimesheetservice;
 	
 	// Ajouter Entreprise : http://localhost:8081/SpringMVC/servlet/ajouterEntreprise
-	//{"id":1,"name":"SSII Consulting","raisonSocial":"Cite El Ghazela"}
+	
 
 	@PostMapping("/ajouterEntreprise")
 	@ResponseBody
@@ -62,7 +62,6 @@ public class RestControlEntreprise {
 	}
     
     // http://localhost:8081/SpringMVC/servlet/ajouterDepartement
- 	//{"id":1,"name":"Telecom"}
 
  	@PostMapping("/ajouterDepartement")
  	@ResponseBody
