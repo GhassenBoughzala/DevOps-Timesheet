5e333a7ecb6aaab5998a16978558a26f7a30b8c9
Amir <Amir.yazidi1@esprit.tn>
2021-10-20 23:56:29 +0100

    suiteBugs

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -1,10 +1,9 @@
 package tn.esprit.spring.services;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
